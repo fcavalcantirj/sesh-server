02451a972dc4716948fcc177d67b79a51ba1461f
fcavalcanti <felipe.cavalcanti.rj@gmail.com>
2014-11-13 11:19:16 -0200

    changing to env variables...

--- server.js
+++ server.js
@@ -3,20 +3,41 @@ var express     = require('express')
     , bodyParser  = require('body-parser');
 
 var User = require('./app/models/user');
-
 var email   = require("./node_modules/emailjs/email");
+
+var hasError = false;
+if(!process.env.EMAILUSER){
+    console.log('missing env variable EMAILUSER');
+    hasError = true;
+}
+if (!process.env.EMAILSMTP){
+    console.log('missing env variable EMAILSMTP');
+    hasError = true;
+}
+if(!process.env.EMAILPASS){
+    console.log('missing env variable EMAILPASS');
+    hasError = true;
+}
+if(!process.env.DBURL){
+    console.log('missing env variable DBURL');
+    hasError = true;
+}
+if(hasError){
+    process.exit(1);
+}
+
 
 var mongo   = require('mongoose');
 var server  = email.server.connect({
-    user:    "hell@sesh.com",
-    password:"password",
-    host:    "smtp.gmail.com",
+    user:    process.env.EMAILUSER,
+    password:process.env.EMAILPASS,
+    host:    process.env.EMAILSMTP,
     ssl:     true
 });
 var sendEmail = function(email) {
     server.send({
         text:    "Welcome!\r\n\r\nYou've been added to the early-access list for Sesh, the world's first online platform for group coaching.\r\n\r\nYou'll be one of the first to receive an invite to join our early adopter groups. We can't wait to let you try Sesh and let us know what you think!\r\n\r\nIf you're as excited as we are, share the love with your friends by posting http://joinsesh.com to Facebook, Twitter, or anywhere you connect.\r\n\r\nIf you have any questions, comments, or other feedback, please send us an email at hello@joinsesh.com. We would love to hear from you.\r\n\r\nThank you,\r\n\r\nThe Sesh Team",
-        from:    "SeshTeam <hello@sesh.com>",
+        from:    "Sesh Team <no-reply@joinsesh.com>",
         to:      email,
         subject: "Welcome to Sesh"
     }, function(err, message) { console.log(err || message); });
@@ -74,11 +95,12 @@ router.route('/users')
                         res.send(err);
                     }
                     //send email
-                    //sendEmail(req.body.email);
+                    sendEmail(req.body.email);
                     res.json({ message: 'success' });
                 });
             }else{
                 //return res.send(400);
+                sendEmail(req.body.email);
                 res.json({ message: 'success' });
             }
         });
